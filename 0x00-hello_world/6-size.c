c18cb84ccbc70573b72b263c504e479266b59ba2
Elduyana <nevafeke@gmail.com>
2022-06-30 19:58:55 -0700

    Task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 cahr  charType;
 int intType;
 long int longintType;
-long long int longlongintType
-float floatType
+long long int longlongintType;
+float floatType;
 printf("Sixe of a char: %d byte(s)\n", sizeof(charType));
 printf("Size of an int: %d byte(s)\n", sizeof(intType));
 printf("Size of a long int: %d byte(s)\n", sizeof(longintType));
