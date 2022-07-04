a8e68c8df768f50437142978372b8e17b51dc34e
Elduyana <nevafeke@gmail.com>
2022-07-04 16:19:12 -0700

    Task 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* Task 2: a program that will print the alphabet in lowercase
+*/
+int main(void)
+{
+int letter;
+for (letter = '97'; letter <= '122'; letter++)
+{
+	putchar(letter);
+}
+	putchar('\n');
+return (0);
+}
