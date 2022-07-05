cb2cf7f1f3f3c5e263504c084479d4a207dea4a7
Elduyana <nevafeke@gmail.com>
2022-07-05 16:59:12 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Main function
+*/
+void print_alphabet_x10(void)
+{
+int l;
+int m;
+for (m = 0; m < 10; m++)
+{
+for (l = 'a'; l <= 'z'; j++)
+{
+_putchar(l);
+}
+_putchar('\n');
+}
+}
+
