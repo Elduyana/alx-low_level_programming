e89f02ed904ce9eaca56436806d892af41a636d1
Elduyana <nevafeke@gmail.com>
2022-07-04 17:34:14 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Variables 0x01, Task 4
+*
+* Return: 0
+*/
+int main(void)
+{
+char l;
+for (l = 'a'; l <= 'z'; l++)
+{
+if (l == 'q' || l == 'e')
+}
+continue;
+}
+putchar (l);
+return (0);
+}
+}
