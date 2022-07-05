f0d0e055f8ec640c2fa4ef5436e8a583ab35261c
Elduyana <nevafeke@gmail.com>
2022-07-04 17:06:19 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,9 @@
 */
 int main(void)
 {
-char letter;
-for (letter = '97'; letter <= '122'; letter++)
-{
-	putchar(letter);
-}
-	putchar('\n');
+char l;
+for (l = 'a'; a <= 'z'; l++)
+putchar(l);
+putchar('\n');
 return (0);
 }
