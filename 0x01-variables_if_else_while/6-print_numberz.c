eb0e0e65cfa703ad5f421189c9fb2e18a6a95b4b
Elduyana <nevafeke@gmail.com>
2022-07-04 18:41:55 -0700

    Task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Variables 0x01, Task 6
+*
+* Return: 0
+*/
+int main(void)
+{
+int n = 0;
+for (n = 0; n < 10; n++)
+putchar('0' + n);
+putchar('\n');
+return (0);
+}
