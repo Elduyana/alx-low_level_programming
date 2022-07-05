af981eff8cbe4a4ca8d7a9c38c2181935a3235a1
Elduyana <nevafeke@gmail.com>
2022-07-04 17:21:51 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Variables 0x01, Task 3
+* Return: 0
+*/
+int main(void)
+{
+char l;
+for (l = 'a'; l <= 'z'; l++)
+putchar(l);
+for (l = 'A'; l <= 'Z'; l++)
+putchar(l);
+putchar('\n');
+return (0);
+}
