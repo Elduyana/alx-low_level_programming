5bd13b4b554a8d5f25c92e0cf90e6e2c50dff1f4
Elduyana <nevafeke@gmail.com>
2022-07-05 16:38:19 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_alphabet - main function Fnctions
+* Return: 0
+*/
+void print_alphabet(void)
+{
+int l;
+for (l = 'a'; l <= 'z'; l++)
+{
+_putchar(l);
+}
+_putchar('\n');
