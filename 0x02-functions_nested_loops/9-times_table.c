c4a8b2cd35a91c160d0c584e3f73272912f48206
Elduyana <nevafeke@gmail.com>
2022-07-06 13:48:37 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,6 @@ for (a = 0; a < 9; a++)
 _putchar('0');
 _putchar(',');
 _putchar(' ');
-}
 for (b = 1; b <= 9; b++)
 {
 c = (a * b);
@@ -24,12 +23,13 @@ else
 _putchar(' ');
 }
 _putchar((c % 10) + '0');
-}
 if (b < 9)
 {
 _putchar(',');
 _putchar(' ');
 }
+}
 _putchar('\n');
 }
 }
+
