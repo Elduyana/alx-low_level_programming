cbcbe02dd5a4daa5913ff39163d48a206ea6bd83
Elduyana <nevafeke@gmail.com>
2022-07-07 07:54:26 -0700

    Task 0

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+void positive_or_negative(int i){
+int n;
+srand(time (0));
+n = i;
+if (n > 0)
+printf("%d is positive\n", n);
+if (n == 0)
+printf("%d is zero\n", n);
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
+
