d96d48514957f9dc7ce709554364dac4df08e683
Elduyana <nevafeke@gmail.com>
2022-07-06 17:46:06 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - function to find and print sum of even-valued numbers
+* Return: 0
+*/
+int main(void)
+{
+int i;
+unsigned long int a, b, next, sum;
+a = 1;
+b = 2;
+sum = 0;
+for (i = 1; i <= 33; i++)
+{
+if (a < 4000000 && (a % 2) == 0)
+{
+sum = sum + s;
+}
+next = j + k;
+j = k;
+k = next;
+}
+printf("%lu\n", sum);
+return (0);
+}
