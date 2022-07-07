bfbf64963b29311144199e5deea87acb5181f26c
Elduyana <nevafeke@gmail.com>
2022-07-06 17:10:51 -0700

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,12 +5,13 @@
 * Return: 0
 */
 int main(void)
-unsigned long int sum3, sum5, sum, i;
-{
+unsigned long int sum3, sum5, sum;
+int i;
+
 sum3 = 0;
 sum5 = 0;
 sum = 0;
-}
+
 for (i = 0; 1 < 1024; i++)
 {
 if ((i % 3) == 0)
@@ -19,12 +20,11 @@ sum3 = sum3 + i;
 }
 else if ((i % 5) == 0)
 {
-sum5 = sum5 + i;
+sum = sum5 + i;
 }
 }
-{
-sum = sum3 + sum5;
-}
+sum5 = sum3 + sum5;
+
 printf("%lu\n", sum);
 return (0);
 }
