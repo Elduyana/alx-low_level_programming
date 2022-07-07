cb359c16c4d7f270ce1eaa27c6f6373c7df38246
Elduyana <nevafeke@gmail.com>
2022-07-07 08:45:33 -0700

    Task 0

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+* positive_or_negative - main function
+* Return: 0
+*/
 
-void positive_or_negative(int i){
+void positive_or_negative(int i)
+{
 int n;
-srand(time (0));
+i = 0;
+srand(time(0));
 n = i;
 if (n > 0)
 printf("%d is positive\n", n);
@@ -16,4 +22,3 @@ printf("%d is negative\n", n);
 }
 return (0);
 }
-
