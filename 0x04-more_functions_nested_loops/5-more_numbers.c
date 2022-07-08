efd5783febf1b6b47eb1de0c53331aa53d342e47
Elduyana <nevafeke@gmail.com>
2022-07-08 16:14:41 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* more_numbers - main function
+*/
+
+void more_numbers(void)
+{
+int a, b;
+for (a = 0; a <= 10; a++)
+for (b = 0; b <= 14; b++)
+{
+_putchar(b + '0');
+}
+_putchar('\n');
+}
