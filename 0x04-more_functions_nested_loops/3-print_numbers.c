5c94b9a07de5e61e8da7f1cef4cfbcfc22dd5828
Elduyana <nevafeke@gmail.com>
2022-07-08 14:44:33 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,10 @@
 
 /**
 * print_numbers - main function
-* Return: 0
-* @i: integer
 */
 void print_numbers(void)
 {
 int i;
 for (i = 0; i < 10; i++)
-_putchar(i + '0');
-_putchar('\n');
+_putchar("%d\n", i);
 }
