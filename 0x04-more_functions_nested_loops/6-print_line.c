30bd3ecf7d133a9803a1e20775c0f99fc5aa3681
Elduyana <nevafeke@gmail.com>
2022-07-08 17:39:22 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_line - main function
+* @n: integer
+*/
+void print_line(int n)
+{
+if (n <= 0)
+_putchar('\n');
+int a;
+for (a = 0; a <= n; a++)
+_putchar('_');
+_putchar('\n');
+}
+
