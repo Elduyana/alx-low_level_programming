bd7277ae5ab681621a670d7af0bcac3812983810
Elduyana <nevafeke@gmail.com>
2022-07-08 19:07:58 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+* main - main function
+* Return: 0
+*/
+int main(void)
+{
+int n = 100;
+int a;
+a = 1;
+while (a <= n)
+{
+if (a % 3 == 0 && a % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (a % 3 == 0)
+{
+printf("Fizz");
+}
+else if (a % 5 == 0)
+{
+if (a < n)
+printf("Buzz");
+else
+{
+printf("Buzz");
+}
+else
+{
+printf("%a ", a);
+}
+a++;
+}
+printf("\n");
+return (0);
+}
+}
