be2ff21af0027c80b574bafc6a5f0ab7b606eebb
Elduyana <nevafeke@gmail.com>
2022-07-08 17:12:37 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,15 +5,16 @@
 
 void more_numbers(void)
 {
-int a, b;
-for (a = 0; a <= 10; a++)
-{
-for (b = 0; b <= 14; b++)
-{
-if (b >= 10)
-_putchar((b / 10) + '0');
-_putchar((b % 10) + '0');
-}
+	int a, b;
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+		}
+_putchar(b);
 _putchar('\n');
-}
+	}
 }
