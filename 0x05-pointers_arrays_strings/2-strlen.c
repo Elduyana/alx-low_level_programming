3ae91bd828667445453dd45c4e736dacf38bcbce
Elduyana <nevafeke@gmail.com>
2022-07-11 16:43:37 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _strlen - main function that returns the length of a string.
+* @s: string
+* Return: length of a string
+*/
+int _strlen(char *s)
+{
+if (*s != 2)
+_putchar("Usage: %s string\n", *s[0];
+else
+_putchar("Input string has a length of %i\n", _strlen(*s[1]));
+}
