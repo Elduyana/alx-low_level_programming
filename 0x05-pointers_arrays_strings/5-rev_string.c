4921a28e8ccf1a69d395f20ca710b6fbe1131b10
Elduyana <nevafeke@gmail.com>
2022-07-11 18:36:28 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* rev_string - main function that reverses a string
+* @s: string
+*/
+void rev_string(char *s)
+{
+int r, len, tmp;
+len = strlen(S);
+for (r = 0; r < len / 2; r++)
+{
+tmp = s[r];
+s[r] = s[len - r - 1];
+s[len - r - 1] = tmp;
+}
+}
