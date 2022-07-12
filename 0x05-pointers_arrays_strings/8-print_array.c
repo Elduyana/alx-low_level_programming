66410ce6f10973b5ea77df12112ade377f823995
Elduyana <nevafeke@gmail.com>
2022-07-11 19:22:50 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.c"
+#include <string.h>
+
+/**
+* print_array - main function to print n elements of an array of integers
+* @a: pointer
+* @n: number of elements of the array
+*/
+void print_array(int *a, int n)
+{
+int b, c;
+for (b = 0; b < n; b++)
+{
+c = *(a + b);
+printf("%d", c);
+if (b != n - 1)
+printf(",");
+}
+printf("\n");
+}
