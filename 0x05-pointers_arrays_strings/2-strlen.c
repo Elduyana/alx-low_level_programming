67ef546b7f0a9d647b200b5426a5fd7d761db551
Elduyana <nevafeke@gmail.com>
2022-07-11 17:03:34 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,10 +2,12 @@
 
 /**
 * _strlen - main function that returns the length of a string.
-* @s: string
+* @*s: string
 * Return: length of a string
 */
 int _strlen(char *s)
 {
+int len;
+char *s string[] = "My first strlen!";
 len = _strlen(char *s);
 }
