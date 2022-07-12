2acb2b1c50399b41acfedbc78f64a5655901f34a
Elduyana <nevafeke@gmail.com>
2022-07-11 17:46:25 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_rev - main function that prints a string in reverse
+* @s: string parameter
+*/
+void print_rev(char *s)
+{
+if (*s)
+{
+reverse(str + 1);
+_putchar("%c", *s);
+}
+return (0);
+}
