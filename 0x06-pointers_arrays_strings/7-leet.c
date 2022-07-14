3863d08a26616ad667ee795c5f9d7b6ccd5c6032
Elduyana <nevafeke@gmail.com>
2022-07-13 18:04:51 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,7 +13,7 @@ while (str[++index1])
 {
 for (index2 = ; index2 <= 7; index2++)
 {
-if (str[index1] == leet[index2] || str[index1 - 32 == leet[index2])
+if (str[index1] == leet[index2] || str[index1] - 32 == leet[index2])
 str[index1] = index2 + '0';
 }
 }
