29f19716ef845b59a8c6155f86b1222afe47e0e1
Elduyana <nevafeke@gmail.com>
2022-07-13 17:20:50 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* string_toupper - lowercase to uppercase
+* @str: parameter
+* Return: string
+*/
+char *string_toupper(char *str)
+{
+int index = 0;
+while (str[index++])
+{
+if (str[index] >= 'a' && str[index <= 'z')
+	str[index] -= 32;
+}
+return (str);
+}
