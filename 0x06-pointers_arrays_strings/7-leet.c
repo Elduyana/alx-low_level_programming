e65375b26deadd16c9e8a867f6b304e7245cce77
Elduyana <nevafeke@gmail.com>
2022-07-13 18:06:42 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,7 +8,7 @@
 char *leet(char *str)
 {
 int index1 = 0, index2;
-char leet[8] = {'A', 'E', '?', 'O', 'T', '?', '?', 'L'};
+char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 while (str[++index1])
 {
 for (index2 = ; index2 <= 7; index2++)
