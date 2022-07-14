c176627981a20ed89a0ff35fd8f37be33a7a87d4
Elduyana <nevafeke@gmail.com>
2022-07-13 18:37:12 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,26 +7,19 @@
 */
 char *cap_string(char *str)
 {
-int index = 0;
-while (str[++index])
-
+char spcc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+int len = 13;
+int a = 0, i;
+while (x[a])
 {
-while (!(str[index] >= 'a' && str[index] <= 'z'))
-index++;
-if (str[index - 1] == ' ' ||
-str[index - 1] == '\t' ||
-str[index - 1] == '\n' ||
-str[index - 1] == ',' ||
-str[index - 1] == ';' ||
-str[index - 1] == '.' ||
-str[index - 1] == '!' ||
-str[index - 1] == '?' ||
-str[index - 1] == '"' ||
-str[index - 1] == '(' ||
-str[index - 1] == ')' ||
-str[index - 1] == '{' ||
-str[index - 1] == '}')
-str[index] -= 32;
+i = 0;
+while (i < len)
+{
+if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+x[a] = x[a] - 32;
+i++;
 }
-return (str);
+a++;
+}
+return (x);
 }
