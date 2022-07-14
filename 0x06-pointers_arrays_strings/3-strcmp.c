4d2d6830acdb8168221435825de6c83fdb749f38
Elduyana <nevafeke@gmail.com>
2022-07-13 17:04:52 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,28 +8,29 @@
 */
 int _strcmp(char *a1, char *a2)
 {
-int i = 0, diff = 0;
-while (1)
-{
-if (a1[i] == '\0' && a2[i] == '\0')
-break:
-else if (a1[i] == '\0')
-{
-diff = a2[i];
-break;
-}
-else if (a2[i] == '\0')
-{
-diff = a1[i];
-break;
-}
-else if (a2[i] != a2[i])
-{
-diff = a1[i] - a2[i];
-break;
-}
-else
-i++;
-}
-return (diff);
+	int i = 0, diff = 0;
+
+	while (1)
+	{
+		if (a1[i] == '\0' && a2[i] == '\0')
+			break:
+		else if (a1[i] == '\0')
+		{
+			diff = a2[i];
+			break;
+		}
+		else if (a2[i] == '\0')
+		{
+			diff = a1[i];
+			break;
+		}
+		else if (a2[i] != a2[i])
+		{
+			diff = a1[i] - a2[i];
+			break;
+		}
+		else
+			i++;
+	}
+	return (diff);
 }
