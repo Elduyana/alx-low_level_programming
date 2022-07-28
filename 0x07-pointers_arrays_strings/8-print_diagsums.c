67a74475f2b4eb49571fe7afb5c14e36be205ce4
Elduyana <nevafeke@gmail.com>
2022-07-28 11:00:37 -0700

    Task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_diagsums - prints ths sum of two diagonals
+* @a: pointer to array
+* @size: size of an array
+* Return: void
+*/
+void print_diagsums(int *a, int size)
+{
+int i, size1;
+unsigned int sumOfdiag1, sumOfdiag2;
+size1 = 0;
+sumOfdiag1 = 0;
+sumOfdiag2 = 0;
+size1 = (size * size) - 1;
+for (i = 0; i <= size1; i = i + (size + 1))
+{
+sumOfdiag1 = sumOfdiag1 + a[i];
+}
+for (i = (size - 1); i < size1; i = i + (size - 1))
+{
+sumOfdiag2 = sumOfdiag2 + a[i];
+}
+printf("%d, %d\n", sumOfdiag1, sumOfddiag2);
+}
