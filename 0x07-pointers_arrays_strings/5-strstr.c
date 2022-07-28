78c3d3c09a0e4dc382406a7688e4a63d52049374
Elduyana <nevafeke@gmail.com>
2022-07-28 10:33:11 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,7 +7,7 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-char *str1, str2;
+char *str1, *str2;
 while (*haystack != '\0')
 {
 str1 = haystack;
@@ -19,7 +19,7 @@ str2++;
 }
 if (*str2 == '\0')
 return (str1);
-haystacka = str1 + 1;
+haystack = str1 + 1;
 }
 return (0);
 }
