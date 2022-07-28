1471b4606d00502bf3170d6f98f13a6cf1089d77
Elduyana <nevafeke@gmail.com>
2022-07-28 09:39:32 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* _memcpy - memory copier 
+* _memcpy - memory copier
 * @dest: source string
 * @n: buffer length
+* @src: memory area
 * Return: new string
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
