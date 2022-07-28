28f124b71055abdf363ef85830972e3b70eddadd
Elduyana <nevafeke@gmail.com>
2022-07-28 10:30:16 -0700

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* _strstr - substring in a string
+* @haystack: mian string
+* @needle: to be searched in haystack
+* Return: 0
+*/
+char *_strstr(char *haystack, char *needle)
+{
+char *str1, str2;
+while (*haystack != '\0')
+{
+str1 = haystack;
+str2 = needle;
+while (haystack != '\0' && *str2 != '\0' && *haystack == *str2)
+{
+haystack++;
+str2++;
+}
+if (*str2 == '\0')
+return (str1);
+haystacka = str1 + 1;
+}
+return (0);
+}
