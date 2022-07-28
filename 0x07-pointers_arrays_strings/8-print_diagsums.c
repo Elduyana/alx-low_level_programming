95b15b12ffeea7e8727c60c4f48696cc2f1d2cd3
Elduyana <nevafeke@gmail.com>
2022-07-28 11:09:08 -0700

    Task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,19 +8,21 @@
 */
 void print_diagsums(int *a, int size)
 {
-int i, size1;
-unsigned int sumOfdiag1, sumOfdiag2;
-size1 = 0;
-sumOfdiag1 = 0;
-sumOfdiag2 = 0;
-size1 = (size * size) - 1;
-for (i = 0; i <= size1; i = i + (size + 1))
-{
-sumOfdiag1 = sumOfdiag1 + a[i];
-}
-for (i = (size - 1); i < size1; i = i + (size - 1))
-{
-sumOfdiag2 = sumOfdiag2 + a[i];
-}
-printf("%d, %d\n", sumOfdiag1, sumOfddiag2);
+	int i, size1;
+	unsigned int sumOfdiag1, sumOfdiag2;
+	size1 = 0;
+	sumOfdiag1 = 0;
+	sumOfdiag2 = 0;
+
+	size1 = (size * size) - 1;
+
+	for (i = 0; i <= size1; i = i + (size + 1))
+	{
+		sumOfdiag1 = sumOfdiag1 + a[i];
+	}
+	for (i = (size - 1); i < size1; i = i + (size - 1))
+	{
+		sumOfdiag2 = sumOfdiag2 + a[i];
+	}
+	printf("%d, %d\n", sumOfdiag1, sumOfddiag2);
 }
