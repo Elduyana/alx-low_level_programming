9f10d70de8db7c278012e8f38aac26708577649b
Elduyana <nevafeke@gmail.com>
2022-07-28 10:43:17 -0700

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_chessboard - function to print chessboard
+* @a: array of pointers
+* Returnn: void
+*/
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i <= 7; i++)
+{
+for (j = 0; j <= 7; j++)
+{
+_putchar(a[i][j])
+}
+_putchar(10);
+}
+}
+
