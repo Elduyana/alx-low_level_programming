8c817196f6ecb2c39c4e2e09284614b9ddd4f9ba
Elduyana <nevafeke@gmail.com>
2022-07-28 17:06:06 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* _putchar - writes the chaaracter c to stdout
+* @c: character
+* Return: 0 or 1
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
