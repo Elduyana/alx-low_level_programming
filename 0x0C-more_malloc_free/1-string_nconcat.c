ee2849ee2cfcdb4dd43367aab03125c19cc18a2b
Elduyana <nevafeke@gmail.com>
2022-07-28 17:54:21 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdlib.h>
 /**
 * _strlen - calculates and returns string length
 * @string: string
@@ -8,8 +7,10 @@
 */
 int _strlen(char *string)
 {
-int i;
-for (i = 0; string[i] != '\0'; i++)
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+
 	return (i);
 }
 /**
@@ -21,26 +22,30 @@ for (i = 0; string[i] != '\0'; i++)
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *ptr:
-int num, len, i, j;
-num = n;
-if (s1 == NULL)
-	s1 = "";
-if (s2 == NULL)
-	s2 = "";
-if (num < 0)
-	return (NULL);
-if (num >= _strlen(s2))
-	num = _strlen(s2);
-len = strlen(s1) + num + 1;
-ptr = maloc(sizeof(*ptr) * len);
-if (ptr == NULL)
-	return (NULL);
-for (i = 0; s1[i] != '\0'; i++)
-	ptr[i] = s1[i];
-for (j = 0; j < num; j++)
-	ptr[i + j] = s2[j];
-ptr[i + j] = '\0';
-return (ptr);
+	char *ptr:
+	int num, len, i, j;
+
+	num = n;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (num < 0)
+		return (NULL);
+	if (num >= _strlen(s2))
+		num = _strlen(s2);
+
+	len = strlen(s1) + num + 1;
+
+	ptr = malloc(sizeof(*ptr) * len);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+	return (ptr);
 }
 
